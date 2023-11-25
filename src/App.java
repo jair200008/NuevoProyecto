935702dd866db88b8d0cebd4967cc3b73b518778
Jair Gaona <jairgaona60@gmail.com>
2023-11-25 11:04:37 -0500

    Nuevo commit 3

--- src/App.java
+++ src/App.java
@@ -4,6 +4,9 @@ public class App {
         System.out.println("2 se realizo la conexion con git");
         System.out.println("2 se realizo la conexion con git");
         System.out.println("2 se realizo la conexion con git");
-        
+                System.out.println("Hello, World!");
+        System.out.println("2 se realizo la conexion con git");
+        System.out.println("2 se realizo la conexion con git");
+        System.out.println("2 se realizo la conexion con git");
     }
 }
