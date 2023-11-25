dbd092289f9765548c5d73572c31d7a9d0eddb97
Jair Gaona <jairgaona60@gmail.com>
2023-11-25 11:01:28 -0500

    Nuevo commit 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("se realizo la conexion con git");
+        System.out.println("2 se realizo la conexion con git");
+        System.out.println("2 se realizo la conexion con git");
+        System.out.println("2 se realizo la conexion con git");
+        
     }
 }
